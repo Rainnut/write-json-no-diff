21f4b95bb425b0ad8db23187472f0736687591da
zhangdongyang <zhangdongyang@joyy.sg>
2021-11-03 20:12:01 +0800

    feat: 初始化项目

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ast2Json
+# ast-to-json
+
+本插件只完成 `ast` 到 `json` 的转换，如需 `json` 转 `ast`，可用 [json-to-ast](https://github.com/vtrushin/json-to-ast)
---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
+export default {
+	input: 'index.js',
+	output: {
+		file: 'build.js',
+		format: 'umd',
+		name: 'astToJson',
+	},
+	plugins: [
+		// Changes package path to relative
+		resolve(),
+		// Convert CommonJS modules to ES6, so they can be included in a Rollup bundle
+		commonjs(),
+		babel()
+	],
+	watch: {
+		include: 'lib/**/*.js',
+		exclude: 'node_modules/**'
+	}
+}
