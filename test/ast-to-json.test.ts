f69b1b95415796c3055d461d388e5a117c44a2ad
zhangdongyang <zhangdongyang@joyy.sg>
2021-11-04 09:53:20 +0800

    feat: init

---
+++ test/ast-to-json.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from "../src/ast-to-json"
+
+/**
+ * Dummy test
+ */
+describe("Dummy test", () => {
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy()
+  })
+
+  it("DummyClass is instantiable", () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
